a295decd1f5b06c14d70321b1fbdb64a63f33519
irinazavaley <185691316+irenzaval@users.noreply.github.com>
2026-05-27 17:04:49 +0300

    setup for backend

---
+++ backend/src/main/java/ru/hh/kakdela_v2/KakdelaV2Application.java
@@ -0,0 +1,13 @@
+package ru.hh.kakdela_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakdelaV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakdelaV2Application.class, args);
+	}
+
+}
