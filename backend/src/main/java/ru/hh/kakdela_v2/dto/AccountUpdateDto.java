01b56a1c832eae912752a734db7fc5e6b1dcd0a7
Esipov Bogdan <bogdanesipov06@mail.ru>
2026-06-02 19:25:15 +0300

    Сделал DTO для обновления профиля

---
+++ backend/src/main/java/ru/hh/kakdela_v2/dto/AccountUpdateDto.java
@@ -0,0 +1,14 @@
+package ru.hh.kakdela_v2.dto;
+
+public class AccountUpdateDto {
+
+  private String login;
+  private String email;
+
+  public AccountUpdateDto() {}
+
+  public String getLogin() { return login; }
+  public String getEmail() { return email; }
+  public void setLogin(String login) { this.login = login; }
+  public void setEmail(String email) { this.email = email; }
+}
