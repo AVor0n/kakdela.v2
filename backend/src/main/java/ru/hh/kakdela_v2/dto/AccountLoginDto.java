a235998aeb1ad013848b0ee66612e55f00f89249
Esipov Bogdan <bogdanesipov06@mail.ru>
2026-06-02 19:30:12 +0300

    Отформатировал DTO для обновления аккаунта и сделал DTO для получения данных авторизации аккаунта и для отдачи токена после успешного входа

---
+++ backend/src/main/java/ru/hh/kakdela_v2/dto/AccountLoginDto.java
@@ -0,0 +1,15 @@
+public class AccountLoginDto {
+  private String login;
+  private String rawPassword;
+
+  public AccountLoginDto() {
+  }
+
+  public String getLogin() {
+    return login;
+  }
+
+  public String getRawPassword() {
+    return rawPassword;
+  }
+}
