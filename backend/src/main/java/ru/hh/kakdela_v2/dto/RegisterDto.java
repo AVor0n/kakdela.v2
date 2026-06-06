5a3110c108fb1580f6c44c4c23e8152436a255dc
Georgiy Podobin <gpodobin@yandex.ru>
2026-06-06 17:55:52 +0300

    Add RegisterDTO

---
+++ backend/src/main/java/ru/hh/kakdela_v2/dto/RegisterDto.java
@@ -0,0 +1,16 @@
+package ru.hh.kakdela_v2.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterDto {
+
+  private String login;
+  private String email;
+  private String rawPassword;
+  private String rawPasswordConfirmation;
+}
