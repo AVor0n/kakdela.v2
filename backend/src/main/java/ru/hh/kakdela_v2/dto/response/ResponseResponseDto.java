37ff4951468c1c0840fff758fb0095cbfb13c3c5
Georgiy Podobin <gpodobin@yandex.ru>
2026-06-13 12:58:57 +0300

    Update ResponseResponseDto: now more relevant Lombok annotation is used

--- backend/src/main/java/ru/hh/kakdela_v2/dto/response/ResponseResponseDto.java
+++ backend/src/main/java/ru/hh/kakdela_v2/dto/response/ResponseResponseDto.java
@@ -1,6 +1,7 @@
 package ru.hh.kakdela_v2.dto.response;
 
 import lombok.Getter;
+import lombok.Value;
 import ru.hh.kakdela_v2.dto.answer.AnswerResponseDto;
 import ru.hh.kakdela_v2.model.Response;
 
@@ -8,18 +9,18 @@ import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
 
-@Getter
+@Value
 public class ResponseResponseDto {
 
-  private final UUID id;
-  private final UUID accountId;   // может быть null — анонимное прохождение
-  private final UUID surveyId;
-  private final boolean isComplete;
-  private final Instant receivedAt;
-  private final List<AnswerResponseDto> answers;
+  UUID responseId;
+  UUID accountId;   // может быть null — анонимное прохождение
+  UUID surveyId;
+  boolean isComplete;
+  Instant receivedAt;
+  List<AnswerResponseDto> answers;
 
   public ResponseResponseDto(Response response) {
-    this.id = response.getId();
+    this.responseId = response.getId();
     this.accountId = response.getAccount() != null
             ? response.getAccount().getId()
             : null;
