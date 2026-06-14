403f336326b76c24471c0cbf1beb2aa5f57d497c
Georgiy Podobin <gpodobin@yandex.ru>
2026-06-14 15:19:21 +0300

    Update custom ValidPassword validation

--- backend/src/main/java/ru/hh/kakdela_v2/util/PasswordConstraintValidator.java
+++ backend/src/main/java/ru/hh/kakdela_v2/util/PasswordConstraintValidator.java
@@ -3,9 +3,9 @@ package ru.hh.kakdela_v2.util;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
-public class PasswordConstraintValidator implements ConstraintValidator<ValidPassword, String> {
+public class PasswordConstraintValidator implements ConstraintValidator<ValidPassword, CharSequence> {
   @Override
-  public boolean isValid(String password, ConstraintValidatorContext context) {
-    return password.matches("^(?=.*[0-9])(?=.*[a-zA-Zа-яА-Я])(?=.*[!@#&()–\\[{}\\]:;',?/*~$^+=<>]).{8,}$");
+  public boolean isValid(CharSequence password, ConstraintValidatorContext context) {
+    return password.toString().matches("^(?=.*[0-9])(?=.*[a-zA-Zа-яА-Я])(?=.*[!@#&()–\\[{}\\]:;',?/*~$^+=<>]).{8,}$");
   }
 }
