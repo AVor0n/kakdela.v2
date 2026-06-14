ac70cda0c21bab181333b68186cf480b9823af91
Georgiy Podobin <gpodobin@yandex.ru>
2026-06-14 16:01:00 +0300

    create_tables.sql: add indices and additional unique constraints

--- backend/src/main/resources/create_tables.sql
+++ backend/src/main/resources/create_tables.sql
@@ -20,6 +20,9 @@ CREATE TABLE survey (
     created_at timestamptz NOT NULL
 );
 
+CREATE INDEX idx_survey_author_id
+ON survey (author_id);
+
 CREATE TABLE permissions (
     account_id uuid REFERENCES account (id) ON DELETE CASCADE,
     survey_id uuid REFERENCES survey (id) ON DELETE CASCADE,
@@ -33,8 +36,12 @@ CREATE TABLE survey_page (
     survey_id uuid REFERENCES survey (id) ON DELETE CASCADE NOT NULL,
     serial_number int NOT NULL,
     title varchar(200),
-    description varchar(5000)
+    description varchar(5000),
+    UNIQUE (survey_id, serial_number)
 );
+
+CREATE INDEX idx_survey_page_survey_id
+ON survey_page (survey_id);
 
 CREATE TABLE question (
     id uuid PRIMARY KEY,
@@ -46,14 +53,19 @@ CREATE TABLE question (
     answer_option_order varchar(255),
     is_mandatory bool NOT NULL DEFAULT true,
     is_visible bool NOT NULL DEFAULT true,
-    condition text
+    condition text,
+    UNIQUE (survey_page_id, serial_number)
 );
+
+CREATE INDEX idx_question_survey_page_id
+ON question (survey_page_id);
 
 CREATE TABLE answer_option (
     id uuid PRIMARY KEY,
     question_id uuid REFERENCES question (id) ON DELETE CASCADE NOT NULL,
     serial_number int NOT NULL,
-    answer_option_text varchar(1000) NOT NULL
+    answer_option_text varchar(1000) NOT NULL,
+    UNIQUE (question_id, serial_number)
 );
 
 CREATE TABLE closing_page (
@@ -71,9 +83,18 @@ CREATE TABLE response (
     received_at timestamptz NOT NULL
 );
 
+CREATE INDEX idx_response_survey_id
+ON response (survey_id);
+
+CREATE idx_response_account_id
+ON response (account_id);
+
 CREATE TABLE answer (
     response_id uuid REFERENCES response (id) ON DELETE CASCADE,
     question_id uuid REFERENCES question (id) ON DELETE CASCADE,
     answer_text varchar(5000) NOT NULL,
     PRIMARY KEY (response_id, question_id)
 );
+
+CREATE INDEX idx_answer_question_id
+ON answer (question_id);
