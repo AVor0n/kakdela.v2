7e47e57a39a55ba07d70a038ee82e701fe1332d3
Georgiy Podobin <gpodobin@yandex.ru>
2026-06-18 17:40:35 +0300

    Add AttachmentController

---
+++ backend/src/main/java/ru/hh/kakdela_v2/controller/AttachmentController.java
@@ -0,0 +1,90 @@
+package ru.hh.kakdela_v2.controller;
+
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import ru.hh.kakdela_v2.dto.object.ObjectUrlResponseDto;
+import ru.hh.kakdela_v2.service.AnswerOptionService;
+import ru.hh.kakdela_v2.service.QuestionService;
+import ru.hh.kakdela_v2.util.CustomUserDetails;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class AttachmentController {
+
+  private final QuestionService questionService;
+  private final AnswerOptionService answerOptionService;
+
+  // Question attachment
+
+  @PostMapping(
+      value = "/questions/{questionId}/attachment",
+      consumes = MediaType.MULTIPART_FORM_DATA_VALUE
+  )
+  @ResponseStatus(HttpStatus.CREATED)
+  public ObjectUrlResponseDto addAttachmentToQuestion(
+      @PathVariable UUID questionId,
+      @RequestParam MultipartFile file,
+      @AuthenticationPrincipal CustomUserDetails currentUser
+  ) {
+    return questionService.addAttachment(
+        questionId,
+        currentUser.getId(),
+        file
+    );
+  }
+
+  @DeleteMapping("/questions/{questionId}/attachment")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void deleteAttachmentFromQuestion(
+      @PathVariable UUID questionId,
+      @AuthenticationPrincipal CustomUserDetails currentUser
+  ) {
+    questionService.deleteAttachment(
+        questionId,
+        currentUser.getId()
+    );
+  }
+
+  // AnswerOption attachment
+
+  @PostMapping(
+      value = "/answer-options/{answerOptionId}/attachment",
+      consumes = MediaType.MULTIPART_FORM_DATA_VALUE
+  )
+  @ResponseStatus(HttpStatus.CREATED)
+  public ObjectUrlResponseDto addAttachmentToAnswerOption(
+      @PathVariable UUID answerOptionId,
+      @RequestParam MultipartFile file,
+      @AuthenticationPrincipal CustomUserDetails currentUser
+  ) {
+    return answerOptionService.addAttachment(
+        answerOptionId,
+        currentUser.getId(),
+        file
+    );
+  }
+
+  @DeleteMapping("/answer-options/{answerOptionId}/attachment")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void deleteAttachmentFromAnswerOption(
+      @PathVariable UUID answerOptionId,
+      @AuthenticationPrincipal CustomUserDetails currentUser
+  ) {
+    answerOptionService.deleteAttachment(
+        answerOptionId,
+        currentUser.getId()
+    );
+  }
+}
