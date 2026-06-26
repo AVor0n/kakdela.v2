33f22ac051f86a514560e9772ea0a1d596015557
artem <artemidusa2006@gmail.com>
2026-06-26 12:29:28 +0300

    add simple mail sending

--- backend/src/main/java/ru/hh/kakdela_v2/controller/SubscriptionController.java
+++ backend/src/main/java/ru/hh/kakdela_v2/controller/SubscriptionController.java
@@ -8,7 +8,7 @@ import ru.hh.kakdela_v2.dto.subscription.SubscriptionRequestDto;
 import ru.hh.kakdela_v2.dto.subscription.SubscriptionResponseDto;
 import ru.hh.kakdela_v2.model.Account;
 import ru.hh.kakdela_v2.service.SurveyNotificationSubscriptionService;
-import ru.hh.kakdela_v2.util.CustomUserDetails;
+import ru.hh.kakdela_v2.security.CustomUserDetails;
 
 import java.util.List;
 import java.util.UUID;
@@ -38,6 +38,7 @@ public class SubscriptionController {
         subscriptionService.unsubscribeUser(surveyId, email, currentUser.getId());
     }
 
+    // TODO: заменить на AccountResponseDto
     @GetMapping("/surveys/{surveyId}/subscribers")
     public List<Account> getSubscribers(
             @PathVariable UUID surveyId,
--- backend/src/main/java/ru/hh/kakdela_v2/service/EmailService.java
+++ backend/src/main/java/ru/hh/kakdela_v2/service/EmailService.java
@@ -1,6 +1,10 @@
 package ru.hh.kakdela_v2.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -9,10 +13,24 @@ import java.util.UUID;
 @Service
 public class EmailService {
 
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
     public void sendSurveyPublishedEmail(String email, String surveyTitle, UUID surveyId) {
         log.info("Уведомление о публикации опроса");
         log.info("Кому: {}", email);
         log.info("Опрос: {}", surveyTitle);
         log.info("Ссылка: /surveys/{}", surveyId);
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(fromEmail);
+        message.setTo(email);
+        message.setSubject("Обязательный опрос \"" + surveyTitle + "\"!");
+        message.setText("Вам необходимо пройти опрос \"" + surveyTitle + "\". Автор указал вас как обязательного респондента.");
+
+        mailSender.send(message);
     }
 }
