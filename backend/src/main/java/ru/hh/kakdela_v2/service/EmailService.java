2ddbe4f68b12de0f6c771acb369c34883b7e1d13
artem <artemidusa2006@gmail.com>
2026-06-26 16:07:21 +0300

    make email notifications send async

--- backend/src/main/java/ru/hh/kakdela_v2/service/EmailService.java
+++ backend/src/main/java/ru/hh/kakdela_v2/service/EmailService.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -19,18 +20,26 @@ public class EmailService {
     @Value("${spring.mail.username}")
     private String fromEmail;
 
+    @Async
     public void sendSurveyPublishedEmail(String email, String surveyTitle, UUID surveyId) {
-        log.info("Уведомление о публикации опроса");
-        log.info("Кому: {}", email);
-        log.info("Опрос: {}", surveyTitle);
-        log.info("Ссылка: /surveys/{}", surveyId);
+        try {
+            log.info("Уведомление о публикации опроса");
+            log.info("Кому: {}", email);
+            log.info("Опрос: {}", surveyTitle);
+            log.info("Ссылка: /surveys/{}", surveyId);
 
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(fromEmail);
-        message.setTo(email);
-        message.setSubject("Обязательный опрос \"" + surveyTitle + "\"!");
-        message.setText("Вам необходимо пройти опрос \"" + surveyTitle + "\". Автор указал вас как обязательного респондента.");
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(fromEmail);
+            message.setTo(email);
+            message.setSubject("Обязательный опрос \"" + surveyTitle + "\"!");
+            message.setText("Вам необходимо пройти опрос \"" + surveyTitle + "\". Автор указал вас как обязательного респондента.");
 
-        mailSender.send(message);
+            mailSender.send(message);
+
+            log.info("Письмо успешно отправлено для: {}", email);
+        }
+        catch (Exception e) {
+            log.error("Ошибка при отправке письма для {}: {}", email, e.getMessage(), e);
+        }
     }
 }
