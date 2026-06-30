ba20b15af8f80abdebc2f549b69d5881bc0ee5f7
Esipov Bogdan <bogdanesipov06@mail.ru>
2026-06-30 15:38:24 +0300

    исправление багов с логированием

--- backend/src/main/java/ru/hh/kakdela/v2/filter/RequestLoggingFilter.java
+++ backend/src/main/java/ru/hh/kakdela/v2/filter/RequestLoggingFilter.java
@@ -10,7 +10,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-import ru.hh.kakdela.v2.util.CustomUserDetails;
+import ru.hh.kakdela.v2.security.CustomUserDetails;
 
 import java.io.IOException;
 import java.util.UUID;
