da2a1a32bca1cc001597faa8a3794515438ceea9
1artem11semenov1 <146671079+1artem11semenov1@users.noreply.github.com>
2026-07-03 13:14:11 +0300

    KD-90 отправка уведомлений для обязательных пользователей (#92)
    * add simple mail sending
    
    * change response format for getSubscribers
    
    * add dev and prod spring profiles
    
    * make email notifications send async
    
    * change account to dto conversion and mailSender injection
    
    * add newline at end of the application.properties
    
    Co-authored-by: Joshua Flooper <75142882+joshuaflooper@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Joshua Flooper <75142882+joshuaflooper@users.noreply.github.com>

--- backend/src/main/java/ru/hh/kakdela/v2/service/EmailService.java
+++ backend/src/main/java/ru/hh/kakdela/v2/service/EmailService.java
@@ -1,18 +1,46 @@
 package ru.hh.kakdela.v2.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class EmailService {
 
+    private final JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
+    @Async
     public void sendSurveyPublishedEmail(String email, String surveyTitle, UUID surveyId) {
-        log.info("Уведомление о публикации опроса");
-        log.info("Кому: {}", email);
-        log.info("Опрос: {}", surveyTitle);
-        log.info("Ссылка: /surveys/{}", surveyId);
+        try {
+            log.info("Уведомление о публикации опроса");
+            log.info("Кому: {}", email);
+            log.info("Опрос: {}", surveyTitle);
+            log.info("Ссылка: /surveys/{}", surveyId);
+
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(fromEmail);
+            message.setTo(email);
+            message.setSubject("Обязательный опрос \"" + surveyTitle + "\"!");
+            message.setText("Вам необходимо пройти опрос \"" + surveyTitle + "\". Автор указал вас как обязательного респондента.");
+
+            mailSender.send(message);
+
+            log.info("Письмо успешно отправлено для: {}", email);
+        }
+        catch (Exception e) {
+            log.error("Ошибка при отправке письма для {}: {}", email, e.getMessage(), e);
+        }
     }
 }
