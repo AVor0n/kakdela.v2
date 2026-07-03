b60abaa5a91f0ae30c040f98a226d19609508246
artem <artemidusa2006@gmail.com>
2026-07-03 13:00:06 +0300

    remove unused import

--- backend/src/main/java/ru/hh/kakdela_v2/service/EmailService.java
+++ backend/src/main/java/ru/hh/kakdela_v2/service/EmailService.java
@@ -2,7 +2,6 @@ package ru.hh.kakdela_v2.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
