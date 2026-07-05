e07f70981a625bdaa3405579988de0e7b712686b
Georgiy Podobin <gpodobin@yandex.ru>
2026-07-05 11:28:50 +0300

    Fix bug: deletion of survey was impossible if connected responses existed

--- backend/src/main/java/ru/hh/kakdela/v2/model/Survey.java
+++ backend/src/main/java/ru/hh/kakdela/v2/model/Survey.java
@@ -1,17 +1,29 @@
 package ru.hh.kakdela.v2.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.OrderBy;
+import jakarta.persistence.Table;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
-
-import java.time.Instant;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
 
 @Data
 @Builder
@@ -69,10 +81,11 @@ public class Survey {
   @Builder.Default
   private List<SurveyPage> pages = new ArrayList<>();
 
-  @OneToOne(mappedBy = "survey", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+  @OneToOne(mappedBy = "survey", cascade = CascadeType.ALL,
+      orphanRemoval = true, fetch = FetchType.LAZY)
   private ClosingPage closingPage;
 
-  @OneToMany(mappedBy = "survey")
+  @OneToMany(mappedBy = "survey", cascade = CascadeType.ALL, orphanRemoval = true)
   @Builder.Default
   private List<Response> responses = new ArrayList<>();
 }
