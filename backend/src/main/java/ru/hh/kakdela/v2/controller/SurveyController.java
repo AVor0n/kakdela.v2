cfa436e14d18aa0b741b758f2fd9bd678ecd9c97
Georgiy Podobin <gpodobin@yandex.ru>
2026-07-09 21:55:55 +0300

    Remove dangerous endpoints, reformat code

--- backend/src/main/java/ru/hh/kakdela/v2/controller/SurveyController.java
+++ backend/src/main/java/ru/hh/kakdela/v2/controller/SurveyController.java
@@ -1,21 +1,27 @@
 package ru.hh.kakdela.v2.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import java.util.List;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
-
-import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import ru.hh.kakdela.v2.dto.survey.SurveyCreateDto;
 import ru.hh.kakdela.v2.dto.survey.SurveyResponseDto;
 import ru.hh.kakdela.v2.dto.survey.SurveyShortResponseDto;
 import ru.hh.kakdela.v2.dto.survey.SurveyUpdateDto;
-import ru.hh.kakdela.v2.service.SurveyService;
 import ru.hh.kakdela.v2.security.CustomUserDetails;
-
-import java.util.List;
-import java.util.UUID;
+import ru.hh.kakdela.v2.service.SurveyService;
 
 @RestController
 @RequestMapping("/api")
@@ -23,59 +29,50 @@ import java.util.UUID;
 @Tag(name = "Surveys", description = "Управление опросами")
 public class SurveyController {
 
-    private final SurveyService surveyService;
+  private final SurveyService surveyService;
 
-    @GetMapping("/surveys/public")
-    public List<SurveyShortResponseDto> getPublished() {
-        return surveyService.getAllPublished();
-    }
+  @GetMapping("/accounts/me/surveys")
+  public List<SurveyShortResponseDto> getMySurveys(
+      @AuthenticationPrincipal CustomUserDetails currentUser) {
+    return surveyService.getMySurveys(currentUser.getId());
+  }
 
-    @GetMapping("/accounts/me/surveys")
-    public List<SurveyShortResponseDto> getMySurveys(@AuthenticationPrincipal CustomUserDetails currentUser) {
-        return surveyService.getMySurveys(currentUser.getId());
-    }
+  @GetMapping("/surveys/{surveyId}")
+  public SurveyResponseDto getById(
+      @PathVariable UUID surveyId,
+      @AuthenticationPrincipal CustomUserDetails currentUser) {
+    return surveyService.getById(surveyId);
+  }
 
-    @GetMapping("/accounts/{authorId}/surveys")
-    public List<SurveyShortResponseDto> getByAuthor(@PathVariable UUID authorId) {
-        return surveyService.getAllByAuthorId(authorId);
-    }
+  @PostMapping("/surveys")
+  @ResponseStatus(HttpStatus.CREATED)
+  public SurveyResponseDto create(
+      @Valid @RequestBody SurveyCreateDto createDto,
+      @AuthenticationPrincipal CustomUserDetails currentUser) {
+    return surveyService.create(currentUser.getId(), createDto);
+  }
 
-    @GetMapping("/surveys/{surveyId}")
-    public SurveyResponseDto getById(
-            @PathVariable UUID surveyId,
-            @AuthenticationPrincipal CustomUserDetails currentUser) {
-        return surveyService.getById(surveyId);
-    }
+  @PutMapping("/surveys/{surveyId}")
+  public SurveyResponseDto update(
+      @PathVariable UUID surveyId,
+      @Valid @RequestBody SurveyUpdateDto updateDto,
+      @AuthenticationPrincipal CustomUserDetails currentUser) {
+    return surveyService.update(surveyId, updateDto, currentUser.getId());
+  }
 
-    @PostMapping("/surveys")
-    @ResponseStatus(HttpStatus.CREATED)
-    public SurveyResponseDto create(
-            @Valid @RequestBody SurveyCreateDto createDto,
-            @AuthenticationPrincipal CustomUserDetails currentUser) {
-        return surveyService.create(currentUser.getId(), createDto);
-    }
+  @PostMapping("/surveys/{surveyId}/clone")
+  @ResponseStatus(HttpStatus.CREATED)
+  public SurveyResponseDto clone(
+      @PathVariable UUID surveyId,
+      @AuthenticationPrincipal CustomUserDetails currentUser) {
+    return surveyService.clone(surveyId, currentUser.getId());
+  }
 
-    @PutMapping("/surveys/{surveyId}")
-    public SurveyResponseDto update(
-            @PathVariable UUID surveyId,
-            @Valid @RequestBody SurveyUpdateDto updateDto,
-            @AuthenticationPrincipal CustomUserDetails currentUser) {
-        return surveyService.update(surveyId, updateDto, currentUser.getId());
-    }
-
-    @PostMapping("/surveys/{surveyId}/clone")
-    @ResponseStatus(HttpStatus.CREATED)
-    public SurveyResponseDto clone(
-            @PathVariable UUID surveyId,
-            @AuthenticationPrincipal CustomUserDetails currentUser) {
-        return surveyService.clone(surveyId, currentUser.getId());
-    }
-
-    @DeleteMapping("/surveys/{surveyId}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete(
-            @PathVariable UUID surveyId,
-            @AuthenticationPrincipal CustomUserDetails currentUser) {
-        surveyService.delete(surveyId, currentUser.getId());
-    }
+  @DeleteMapping("/surveys/{surveyId}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void delete(
+      @PathVariable UUID surveyId,
+      @AuthenticationPrincipal CustomUserDetails currentUser) {
+    surveyService.delete(surveyId, currentUser.getId());
+  }
 }
