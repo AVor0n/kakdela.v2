75a8aab68b646087db2f5d0f22499b7776e6a040
Esipov Bogdan <bogdanesipov06@mail.ru>
2026-07-17 23:56:40 +0300

    добавлена проверка в AuthService на providedPassword

--- backend/src/main/java/ru/hh/kakdela/v2/service/AuthService.java
+++ backend/src/main/java/ru/hh/kakdela/v2/service/AuthService.java
@@ -31,6 +31,9 @@ public class AuthService {
   }
 
   public void checkPassword(UserDetails userDetails, String providedPassword) {
+    if (providedPassword == null) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Текущий пароль обязателен");
+    }
     if (!passwordEncoder.matches(providedPassword, userDetails.getPassword())) {
       throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Предоставлен неверный пароль");
     }
