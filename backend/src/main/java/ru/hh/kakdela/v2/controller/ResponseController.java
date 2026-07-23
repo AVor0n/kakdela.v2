9f73a4cffdf3e3ea5649be39c136813830b76e4b
1artem11semenov1 <146671079+1artem11semenov1@users.noreply.github.com>
2026-07-24 00:11:27 +0300

    KD-139: выгрузка ответов в excel (#146)
    * add responses excel export
    
    * checkstyle fix
    
    * refactor code, add new dto
    
    * merge main, change permission check in export
    
    * rename dto, change user display in file
    
    * merge main, refactor with new response dto

--- backend/src/main/java/ru/hh/kakdela/v2/controller/ResponseController.java
+++ backend/src/main/java/ru/hh/kakdela/v2/controller/ResponseController.java
@@ -3,11 +3,15 @@ package ru.hh.kakdela.v2.controller;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,9 +21,11 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import ru.hh.kakdela.v2.constants.CookieNames;
 import ru.hh.kakdela.v2.dto.response.ResponseCreateResponseDto;
+import ru.hh.kakdela.v2.dto.response.ResponseExportDto;
 import ru.hh.kakdela.v2.dto.response.ResponseResponseDto;
 import ru.hh.kakdela.v2.dto.response.ResponseWithTokenDto;
 import ru.hh.kakdela.v2.security.CustomUserDetails;
+import ru.hh.kakdela.v2.service.ResponseExportService;
 import ru.hh.kakdela.v2.service.ResponseService;
 import ru.hh.kakdela.v2.util.CookieUtil;
 
@@ -30,6 +36,7 @@ import ru.hh.kakdela.v2.util.CookieUtil;
 public class ResponseController {
 
   private final ResponseService responseService;
+  private final ResponseExportService exportService;
 
   @Value("${app.tokens.response-access.max-age}")
   private long responseTokenMaxAge;
@@ -127,6 +134,26 @@ public class ResponseController {
   ) {
 
     return responseService.getCompletedBySurveyId(surveyId, currentUser.getId());
+  }
+
+  @GetMapping("/surveys/{surveyId}/responses/export")
+  public ResponseEntity<byte[]> exportSurveyResponses(
+      @PathVariable UUID surveyId,
+      @AuthenticationPrincipal CustomUserDetails currentUser
+  ) {
+
+    ResponseExportDto excelData = responseService.export(surveyId, currentUser.getId());
+
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+    headers.set(HttpHeaders.CONTENT_DISPOSITION,
+        String.format("attachment; filename=\"%s\"; filename*=UTF-8''%s",
+            excelData.getFilename(), excelData.getFilename()));
+    headers.setContentLength(excelData.getFile().length);
+
+    return ResponseEntity.ok()
+        .headers(headers)
+        .body(excelData.getFile());
   }
 
 }
