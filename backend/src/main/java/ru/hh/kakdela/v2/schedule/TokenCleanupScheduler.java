3ddb347e062ad692188003721e471f9ae65a7ad9
irinazavaley <185691316+irenzaval@users.noreply.github.com>
2026-07-25 01:35:27 +0300

    add scheduler for clearing cookies and setting for pool

---
+++ backend/src/main/java/ru/hh/kakdela/v2/schedule/TokenCleanupScheduler.java
@@ -0,0 +1,26 @@
+package ru.hh.kakdela.v2.schedule;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import ru.hh.kakdela.v2.service.RefreshTokenService;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class TokenCleanupScheduler {
+
+  private final RefreshTokenService refreshTokenService;
+
+  //"0 0 3 * * ?" = каждый день в 3:00
+  @Scheduled(cron = "0 0 3 * * ?", zone = "Europe/Moscow")
+  public void cleanExpiredTokens() {
+    try {
+      refreshTokenService.cleanExpiredTokens();
+      log.info("Очистка истекших refresh токенов завершена");
+    } catch (Exception e) {
+      log.error(" Ошибка при очистке истекших refresh токенов", e);
+    }
+  }
+}
