58843ac6f99c6701cdfcdd3683b60c53b4a0a18e
irinazavaley <185691316+irenzaval@users.noreply.github.com>
2026-07-29 20:23:30 +0300

    fix error after merge

--- backend/src/main/java/ru/hh/kakdela/v2/dto/survey/SurveyResponseDto.java
+++ backend/src/main/java/ru/hh/kakdela/v2/dto/survey/SurveyResponseDto.java
@@ -8,8 +8,9 @@ import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.ToString
+import lombok.ToString;
 import ru.hh.kakdela.v2.dto.account.AccountResponseDto;
+import ru.hh.kakdela.v2.dto.closing.ClosingPageResponseDto;
 import ru.hh.kakdela.v2.dto.survey.page.SurveyPageResponseDto;
 
 @AllArgsConstructor
