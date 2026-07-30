6f9d312e297752e7534267dd451b60865c5e2db5
Georgiy Podobin <gpodobin@yandex.ru>
2026-07-30 19:16:31 +0300

    Update db schema, add migrations, add dao for conditions

---
+++ backend/src/main/java/ru/hh/kakdela/v2/dao/ConditionNodeDaoImpl.java
@@ -0,0 +1,33 @@
+package ru.hh.kakdela.v2.dao;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import java.util.Optional;
+import java.util.UUID;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+import ru.hh.kakdela.v2.model.condition.ConditionNode;
+
+@Repository
+public class ConditionNodeDaoImpl implements ConditionNodeDao {
+
+  @PersistenceContext
+  private EntityManager entityManager;
+
+  @Override
+  public Optional<ConditionNode> findById(UUID id) {
+    return Optional.ofNullable(entityManager.find(ConditionNode.class, id));
+  }
+
+  public void save(ConditionNode conditionNode) {
+    entityManager.persist(conditionNode);
+  }
+
+  public void update(ConditionNode conditionNode) {
+    entityManager.merge(conditionNode);
+  }
+
+  public void delete(ConditionNode conditionNode) {
+    entityManager.remove(conditionNode);
+  }
+}
---
+++ backend/src/main/java/ru/hh/kakdela/v2/model/condition/ConditionNode.java
@@ -0,0 +1,77 @@
+package ru.hh.kakdela.v2.model.condition;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "condition_node")
+public class ConditionNode {
+
+  @Id
+  @Column(name = "id")
+  private UUID id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "condition_id", nullable = false)
+  @OnDelete(action = OnDeleteAction.CASCADE)
+  private Condition condition;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "parent_node_id")
+  private ConditionNode parentNode;
+
+  @Column(name = "operator", nullable = false)
+  @Enumerated(EnumType.STRING)
+  private Operator operator;
+
+  @Column(name = "height", nullable = false)
+  private Integer height;
+
+  @OneToOne(
+      mappedBy = "node",
+      fetch = FetchType.LAZY,
+      cascade = CascadeType.ALL,
+      orphanRemoval = true)
+  private ConditionAtom atom;
+
+  @OneToMany(
+      mappedBy = "parentNode",
+      fetch = FetchType.LAZY,
+      cascade = CascadeType.ALL,
+      orphanRemoval = true)
+  @Builder.Default
+  private List<ConditionNode> childNodes = new ArrayList<>();
+
+  @AllArgsConstructor(access = AccessLevel.PRIVATE)
+  public enum Operator {
+    AND(true),
+    OR(true),
+    ATOM(false);
+
+    public final boolean isLink;
+  }
+}
