b348c690e2034db4b9c8aaa7657fc1f763ff4f83
Georgiy Podobin <gpodobin@yandex.ru>
2026-07-30 19:16:31 +0300

    Update db schema, add migrations, add dao for conditions

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -46,6 +46,9 @@ CREATE TABLE survey_page (
 CREATE INDEX idx_survey_page_survey_id
 ON survey_page (survey_id);
 
+CREATE INDEX idx_survey_page_survey_id_survey_serial_number
+ON survey_page(survey_id, serial_number);
+
 CREATE TABLE question (
     id uuid PRIMARY KEY,
     survey_page_id uuid REFERENCES survey_page (id) ON DELETE CASCADE NOT NULL,
@@ -124,6 +127,37 @@ CREATE TABLE selected_answer_option (
 
 CREATE INDEX idx_selected_answer_option_answer_id
 ON selected_answer_option (answer_id);
+
+CREATE TABLE condition (
+    id uuid PRIMARY KEY,
+    survey_page_id uuid REFERENCES survey_page (id) ON DELETE CASCADE NOT NULL,
+    next_page_id uuid REFERENCES survey_page (id) ON DELETE CASCADE NOT NULL,
+    root_node_id uuid UNIQUE
+);
+
+CREATE INDEX idx_condition_survey_page_id
+ON condition (survey_page_id);
+
+CREATE TABLE condition_node (
+    id uuid PRIMARY KEY,
+    condition_id uuid REFERENCES condition (id) ON DELETE CASCADE NOT NULL,
+    parent_node_id uuid REFERENCES condition_node (id),
+    operator varchar(255) NOT NULL,
+    height int NOT NULL
+);
+
+ALTER TABLE condition
+ADD CONSTRAINT fk_condition_root_node
+FOREIGN KEY (root_node_id)
+REFERENCES condition_node (id);
+
+CREATE TABLE condition_atom (
+    condition_node_id uuid PRIMARY KEY REFERENCES condition_node (id) ON DELETE CASCADE,
+    question_id uuid REFERENCES question (id) ON DELETE CASCADE NOT NULL,
+    operator varchar(255) NOT NULL,
+    required_boolean_value bool,
+    required_answer_option_id uuid REFERENCES answer_option (id) ON DELETE CASCADE
+);
 
 CREATE TABLE survey_notification_subscription (
     survey_id uuid REFERENCES survey (id) ON DELETE CASCADE NOT NULL,
