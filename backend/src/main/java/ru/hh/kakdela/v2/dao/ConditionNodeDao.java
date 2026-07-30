620c44c76309e7d710ecef84b4df69fbb6c612ed
Georgiy Podobin <gpodobin@yandex.ru>
2026-07-30 19:16:31 +0300

    Update db schema, add migrations, add dao for conditions

---
+++ backend/src/main/java/ru/hh/kakdela/v2/dao/ConditionNodeDao.java
@@ -0,0 +1,17 @@
+package ru.hh.kakdela.v2.dao;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import ru.hh.kakdela.v2.model.condition.ConditionNode;
+
+public interface ConditionNodeDao {
+
+  Optional<ConditionNode> findById(UUID id);
+
+  void save(ConditionNode conditionNode);
+
+  void update(ConditionNode conditionNode);
+
+  void delete(ConditionNode conditionNode);
+}
