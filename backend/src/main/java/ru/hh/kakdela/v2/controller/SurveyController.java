d96941bbe652dd509551d0f78c735b0308803c56
1artem11semenov1 <146671079+1artem11semenov1@users.noreply.github.com>
2026-08-02 23:19:10 +0300

    KD-173: ручка получения назначенных опросов (#176)
    * add endpoint for get assigned surveys for user
    
    * fix tests
    
    * checkstyle fix (imports style)
    
    * add empty string in dto class
    
    Co-authored-by: N1GHTWAREN <158213579+N1GHTWAREN@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: N1GHTWAREN <158213579+N1GHTWAREN@users.noreply.github.com>

--- backend/src/main/java/ru/hh/kakdela/v2/controller/SurveyController.java
+++ backend/src/main/java/ru/hh/kakdela/v2/controller/SurveyController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import ru.hh.kakdela.v2.dto.survey.SurveyCreateDto;
 import ru.hh.kakdela.v2.dto.survey.SurveyResponseDto;
+import ru.hh.kakdela.v2.dto.survey.SurveyShortResponseDto;
 import ru.hh.kakdela.v2.dto.survey.SurveyShortResponseWithPermissionDto;
 import ru.hh.kakdela.v2.dto.survey.SurveyUpdateDto;
 import ru.hh.kakdela.v2.security.CustomUserDetails;
@@ -36,6 +37,13 @@ public class SurveyController {
       @AuthenticationPrincipal CustomUserDetails currentUser
   ) {
     return surveyService.getMySurveys(currentUser.getId());
+  }
+
+  @GetMapping("/accounts/me/surveys/assigned")
+  public List<SurveyShortResponseDto> getMyAssignedSurveys(
+      @AuthenticationPrincipal CustomUserDetails currentUser
+  ) {
+    return surveyService.getMyAssignedSurveys(currentUser.getId());
   }
 
   @GetMapping("/surveys/{surveyId}")
