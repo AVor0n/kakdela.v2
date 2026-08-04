fee2b314b7fe36b72e598ca920559f0dc7488fb5
Esipov Bogdan <bogdanesipov06@mail.ru>
2026-08-04 19:38:58 +0300

    checkstyle fix

--- backend/src/main/java/ru/hh/kakdela/v2/model/Account.java
+++ backend/src/main/java/ru/hh/kakdela/v2/model/Account.java
@@ -1,7 +1,7 @@
 package ru.hh.kakdela.v2.model;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
