202dbeabd35f6580225c2f078dc57f5b470187f4
Georgiy Podobin <gpodobin@yandex.ru>
2026-08-06 15:03:05 +0300

    Fix NPE, add additional check, fix method definition and calling

--- backend/src/main/java/ru/hh/kakdela/v2/dao/ConditionNodeDaoImpl.java
+++ backend/src/main/java/ru/hh/kakdela/v2/dao/ConditionNodeDaoImpl.java
@@ -4,7 +4,6 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import java.util.Optional;
 import java.util.UUID;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 import ru.hh.kakdela.v2.model.condition.ConditionNode;
 
@@ -19,14 +18,17 @@ public class ConditionNodeDaoImpl implements ConditionNodeDao {
     return Optional.ofNullable(entityManager.find(ConditionNode.class, id));
   }
 
+  @Override
   public void save(ConditionNode conditionNode) {
     entityManager.persist(conditionNode);
   }
 
+  @Override
   public void update(ConditionNode conditionNode) {
     entityManager.merge(conditionNode);
   }
 
+  @Override
   public void delete(ConditionNode conditionNode) {
     entityManager.remove(conditionNode);
   }
