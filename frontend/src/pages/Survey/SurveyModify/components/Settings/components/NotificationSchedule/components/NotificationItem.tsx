170e7d8798d542d11b2088ff1d5292e3e851bf89
Kirill <gardpen737@gmail.com>
2026-08-09 14:53:10 +0300

    Simple refactor

--- frontend/src/pages/Survey/SurveyModify/components/Settings/components/NotificationSchedule/components/NotificationItem.tsx
+++ frontend/src/pages/Survey/SurveyModify/components/Settings/components/NotificationSchedule/components/NotificationItem.tsx
@@ -10,67 +10,17 @@ import {
 } from '@/entities/NotificationSchedule/NotificationSchedule.slice';
 import { setErrorMessage } from '@/entities/Error/Error.slice';
 import classNames from 'classnames';
-
+import {
+    DAYS,
+    DAYS_VALUE,
+    formatHM,
+    getDaysFromNumber,
+    getScheduleTypeByValue,
+    NOTIFICATION_TYPE_SELECT_DATA,
+    type Day,
+} from './utils';
 interface Props {
     notificationSchedule: NotificationSchedule;
-}
-
-const NOTIFICATION_TYPE_SELECT_DATA = [
-    { text: 'Ежедневно', value: 'DAILY' },
-    { text: 'Еженедельно', value: 'WEEKLY' },
-    { text: 'Ежемесячно', value: 'MONTHLY' },
-];
-
-const DAYS = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
-type Day = (typeof DAYS)[number];
-
-const DAYS_VALUE: Record<Day, number> = {
-    Пн: 0b00000001,
-    Вт: 0b00000010,
-    Ср: 0b00000100,
-    Чт: 0b00001000,
-    Пт: 0b00010000,
-    Сб: 0b00100000,
-    Вс: 0b01000000,
-};
-
-const VALUE_DAYS: Record<string, Day> = {
-    0b00000001: 'Пн',
-    0b00000010: 'Вт',
-    0b00000100: 'Ср',
-    0b00001000: 'Чт',
-    0b00010000: 'Пт',
-    0b00100000: 'Сб',
-    0b01000000: 'Вс',
-};
-
-function getScheduleTypeByValue(scheduleType: ScheduleType) {
-    return NOTIFICATION_TYPE_SELECT_DATA.find((st) => st.value === scheduleType);
-}
-
-function formatHM(value: string): string {
-    const [hh, mm] = value.split(':').map(Number);
-
-    const date = new Date();
-    date.setHours(hh, mm, 0, 0);
-    return `${date.getHours()}:${date.getMinutes() > 9 ? date.getMinutes() : '0' + date.getMinutes()}`;
-}
-
-function getBinNumber(n: number): string {
-    return n.toString(2).padStart(8, '0');
-}
-
-function getDaysFromNumber(n: number): string[] {
-    if (n > 127) return [];
-    const binNum = getBinNumber(n);
-    const result: Day[] = [];
-    for (let i = 1; i < binNum.length; i++) {
-        if (binNum[i] === '0') continue;
-        const value = parseInt('0'.repeat(i) + '1' + '0'.repeat(7 - i), 2);
-        result.push(VALUE_DAYS[value]);
-    }
-
-    return result;
 }
 
 export function NotificationItem({ notificationSchedule }: Props) {
@@ -218,7 +168,11 @@ export function NotificationItem({ notificationSchedule }: Props) {
             </section>
 
             <div className={style.content}>
-                <Button mode='secondary' style='neutral' onClick={() => updateScheduleActiveHandler(!isActive)}>
+                <Button
+                    mode='secondary'
+                    style={isActive ? 'accent' : 'negative'}
+                    onClick={() => updateScheduleActiveHandler(!isActive)}
+                >
                     {isActive ? 'Активное' : 'Не активно'}
                 </Button>
                 <Button
