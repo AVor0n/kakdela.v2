9d5fbf6b0973a9c53a0b7a54768f374b87d7e424
Georgiy Podobin <gpodobin@yandex.ru>
2026-08-12 16:13:52 +0300

    improve custom exception handling

--- backend/src/main/java/ru/hh/kakdela/v2/exception/handler/GlobalExceptionHandler.java
+++ backend/src/main/java/ru/hh/kakdela/v2/exception/handler/GlobalExceptionHandler.java
@@ -19,6 +19,7 @@ import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 import ru.hh.kakdela.v2.dto.error.ErrorResponse;
 import ru.hh.kakdela.v2.exception.ErrorCode;
+import ru.hh.kakdela.v2.exception.Kd2Exception;
 import ru.hh.kakdela.v2.exception.ResetCodeException;
 import ru.hh.kakdela.v2.exception.response.NotAllMandatoryQuestionsAnsweredException;
 import ru.hh.kakdela.v2.mapper.ErrorMapper;
@@ -27,13 +28,13 @@ import ru.hh.kakdela.v2.mapper.ErrorMapper;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-  @ExceptionHandler(NotAllMandatoryQuestionsAnsweredException.class)
-  public ResponseEntity<ErrorResponse> handleNotAllMandatoryQuestionsAnsweredException(
+  @ExceptionHandler(Kd2Exception.class)
+  public ResponseEntity<ErrorResponse> handleKd2Exception(
       NotAllMandatoryQuestionsAnsweredException ex, WebRequest request
   ) {
     UUID id = UUID.randomUUID();
     return ResponseEntity
-        .status(HttpStatus.CONFLICT)
+        .status(ex.getHttpStatus())
         .body(ErrorMapper.getErrorResponse(id, ex, request));
   }
 
--- backend/src/main/java/ru/hh/kakdela/v2/exception/response/NotAllMandatoryQuestionsAnsweredException.java
+++ backend/src/main/java/ru/hh/kakdela/v2/exception/response/NotAllMandatoryQuestionsAnsweredException.java
@@ -1,5 +1,6 @@
 package ru.hh.kakdela.v2.exception.response;
 
+import org.springframework.http.HttpStatus;
 import ru.hh.kakdela.v2.exception.ErrorCode;
 import ru.hh.kakdela.v2.exception.Kd2Exception;
 
@@ -7,6 +8,7 @@ public class NotAllMandatoryQuestionsAnsweredException extends Kd2Exception {
 
   public NotAllMandatoryQuestionsAnsweredException() {
     super(ErrorCode.NOT_ALL_MANDATORY_QUESTIONS_ANSWERED,
+        HttpStatus.CONFLICT,
         "Не все обязательные вопросы заполнены");
   }
 }
