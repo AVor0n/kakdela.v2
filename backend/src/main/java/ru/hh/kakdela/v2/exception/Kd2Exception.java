e8b5a6204678f3c854a02b99fdd651ab842e90ff
Georgiy Podobin <gpodobin@yandex.ru>
2026-08-12 16:13:52 +0300

    improve custom exception handling

--- backend/src/main/java/ru/hh/kakdela/v2/exception/Kd2Exception.java
+++ backend/src/main/java/ru/hh/kakdela/v2/exception/Kd2Exception.java
@@ -1,14 +1,17 @@
 package ru.hh.kakdela.v2.exception;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 public class Kd2Exception extends RuntimeException {
 
   private final ErrorCode errorCode;
+  private final HttpStatus httpStatus;
 
-  protected Kd2Exception(ErrorCode errorCode, String message) {
+  protected Kd2Exception(ErrorCode errorCode, HttpStatus httpStatus, String message) {
     super(message);
     this.errorCode = errorCode;
+    this.httpStatus = httpStatus;
   }
 }
