64e5f696c50693d12892951bc6b7eac03398e3fb
Georgiy Podobin <gpodobin@yandex.ru>
2026-08-14 03:19:33 +0300

    Fix response page status update when answer create and delete

--- backend/src/main/java/ru/hh/kakdela/v2/service/QuestionService.java
+++ backend/src/main/java/ru/hh/kakdela/v2/service/QuestionService.java
@@ -324,7 +324,17 @@ public class QuestionService {
 
   // Вспомогательные методы
 
+  Question getEntityById(UUID id) {
+    return questionDao.findById(id)
+        .orElseThrow(() -> new ResponseStatusException(
+            HttpStatus.NOT_FOUND, "Вопрос не найден: " + id));
+  }
+
   UUID getParentSurveyIdById(UUID id) {
     return questionDao.findParentSurveyIdById(id);
+  }
+
+  UUID getParentPageIdById(UUID id) {
+    return questionDao.findParentPageIdById(id);
   }
 }
