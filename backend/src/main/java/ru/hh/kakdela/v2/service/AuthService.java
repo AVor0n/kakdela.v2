58ef7ea8fbbe38de154699a9ec6aff156c8a972e
Esipov Bogdan <bogdanesipov06@mail.ru>
2026-08-18 01:09:52 +0300

    maven fix

--- backend/src/main/java/ru/hh/kakdela/v2/service/AuthService.java
+++ backend/src/main/java/ru/hh/kakdela/v2/service/AuthService.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
